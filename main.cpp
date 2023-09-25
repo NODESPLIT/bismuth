b707f0fb7b81e2bcc400ef19826a496150d28c20
anom.li <jon@anom.li>
2023-09-25 23:36:52 +0100

    Updated file loading and flags

--- main.cpp
+++ main.cpp
@@ -14,15 +14,18 @@ int main(int argc, char *argv[]) {
 		{ "-q", false }, { "--quiet", false }
 	};
 
-	int skip = 1;
-	for (int i = 1; i < args.size(); i++) if (flags.count(args[i])) { flags[args[i]] = true; skip = i + 1; }
+	string path = "./";
+	for (int i = 1; i < args.size(); i++) {
+		if (flags.count(args[i])) {
+			flags[args[i]] = true;
+		} else {
+			path = args[i];
+		}
+	}
 
-	bool verbose = flags["-v"] || flags["--verbose"];
 	bool repl = flags["-r"] || flags["--repl"];
 	bool quiet = flags["-q"] || flags["--quiet"];
-
-	string path = "./";
-	if (skip < args.size()) path = args[skip];
+	bool verbose = flags["-v"] || flags["--verbose"];
 
 	if (repl || ( args.size() <= 1 && !Bismuth::Exists(path) )) {
 		Bismuth::Repl();
@@ -35,5 +38,5 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
 }
